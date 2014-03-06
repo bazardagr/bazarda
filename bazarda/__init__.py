bad64ff5d0957002b83ad5646662da84b62db28b
boboss <costas@codestar.gr>
2014-03-07 01:45:31 +0200

    add config from object :D

--- bazarda/__init__.py
+++ bazarda/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template
+
 app = Flask(__name__)
+app.config.from_object('bazarda.config_app.DevelopmentConfig') #change this on production
 
 from bazarda import views
--- runserver.py
+++ runserver.py
@@ -1,3 +1,3 @@
 from bazarda import app
 
-app.run(debug=True)
+app.run()
